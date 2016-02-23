849e7af2ffb04b0380c4a495e73d423995ec837a
zabrinaisis <zabrinaisis@gmail.com>
2016-02-23 17:40:33 -0600

    new file:   W1D2/countdown 	new file:   W1D2/countdown.c

---
+++ W1D2/countdown.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    
+    int count = 10;
+
+    while (count > 0) {
+        printf("%d\n", count);
+        count = count - 1;
+    }   
+
+    printf("0\n");
+    printf("Blastoff!!!\n");
+
+}
