9827a1a7d0e0dae933208d1fe2a63f3a1188d67c
zabrinaisis <zabrinaisis@gmail.com>
2016-02-24 19:23:23 -0600

    W1D3

---
+++ W1D3/uppercase.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+   int c;
+   int upper(int c);
+   
+   while ((c = getchar()) != EOF) {
+       putchar(upper(c));
+   }
+}
+
+int upper(int c) {
+    if (c >= 'a' && c <= 'z')
+        return(c - ' ');
+    else return(c);
+}
